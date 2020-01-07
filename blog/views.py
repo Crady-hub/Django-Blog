6cb5c215582c319ffcc5262830570ee6e15f837d
Dmitry <Crady-hub@users.noreply.github.com>
2020-01-07 12:23:49 +0200

    Django

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Posts
+
+def home(request):
+    context = {
+        'posts': Posts.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
